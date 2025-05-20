7ea2704b90cd231e6faeeb2524f97293ce7d18fd
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-20 15:21:35 -0300

    Initializing Loop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String nome = "Vinícius Freiy";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("***********************************");
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo da Conta: " + tipoConta);
+        System.out.println("Saldo Atual: " + saldo);
+        System.out.println("***********************************");
+
+        Scanner input = new Scanner(System.in);
+        int opcao = -1;
+        String menu = """
+                ***** Digite sua Opção *****
+                1 - Consultar Saldo
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                ****************************
+                """;
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = input.nextInt();
+        }
     }
 }
